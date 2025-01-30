07aff9f16a6f58d823a1e09c005ec4f62987e1c3
Richard Chou <konami99@hotmail.com>
2025-01-30 22:51:07 +1100

    updated habit calendar

--- app/(main)/goal/[id].tsx
+++ app/(main)/goal/[id].tsx
@@ -1,7 +1,7 @@
 import { View, Text, Pressable } from "react-native"
 import BackButton from "@/components/BackButton"
 import { useRouter } from "expo-router";
-import { Calendar, CalendarUtils } from 'react-native-calendars';
+import { Calendar, CalendarUtils, DateData } from 'react-native-calendars';
 import ScreenWrapper from "@/components/ScreenWrapper";
 import { SetStateAction, useCallback, useEffect, useMemo, useState } from "react";
 import { getDaysOfCurrentWeek } from "@/helpers/common";
@@ -21,37 +21,18 @@ const GoalEdit = () => {
   const [goalsAndCompletes, setGoalsAndCompletes] = useState([])
   const { id } = useLocalSearchParams<{ id: string }>();
 
-  const onDayPress = useCallback((day: { dateString: SetStateAction<string>; }) => {
-    console.log('onDayPress: ', day);
-    setSelected(day.dateString);
-  }, []);
-
   const getDate = (count: number) => {
+    console.log('getdate')
     const date = new Date(INITIAL_DATE);
     const newDate = date.setDate(date.getDate() + count);
     return CalendarUtils.getCalendarDateString(newDate);
   };
 
-  const marked = useMemo(() => {
-    return {
-      [getDate(-1)]: {
-        dotColor: 'red',
-        marked: true
-      },
-      [selected]: {
-        selected: true,
-        disableTouchEvent: true,
-        selectedColor: 'orange',
-        selectedTextColor: 'red'
-      }
-    };
-  }, [selected]);
-
   const dates = getDaysOfCurrentWeek();
 
-  const fetchGoals = async (userId: string) => {
+  const fetchGoals = async (userId: string, some_date: string) => {
     setGoalsAndCompletes([]);
-    let { data, error } = await supabase.rpc('getgoalsandcompletesfromgoalid', { goalid: id, user_id: userId })
+    let { data, error } = await supabase.rpc('getgoalsandcompletesfromgoalid2', { goalid: id, user_id: userId, some_date })
     if (error) console.error(error)
     else console.log(data);
 
@@ -60,7 +41,7 @@ const GoalEdit = () => {
 
   useEffect(() => {
     if (user?.id) {
-      fetchGoals(user.id);
+      fetchGoals(user.id, INITIAL_DATE);
     }
   }, []);
 
@@ -79,10 +60,13 @@ const GoalEdit = () => {
       else if (dates.includes(date.dateString)) {
         return <RadioButton key={ index } isOnInit={ false } goalId={ goal?.id } date={ date.dateString } style="mt-0.5" />
       }
+      else if (!dates.includes(date.dateString) && completed_dates && completed_dates.includes(date.dateString)) {
+        return <View className={ `h-4 w-4 rounded-lg border-2 border-gray-500 bg-gray-500`} />
+      }
     }
 
     return <View>
-      <Text>{ date.day }</Text>
+      <Text className="text-gray-500">{ date.day }</Text>
     </View>
   };
 
@@ -93,15 +77,19 @@ const GoalEdit = () => {
   return (
     <ScreenWrapper bg={"white"}>
       <View className="m-4 mt-2">
-        <View className="flex flex-row justify-end">
+        <View className="m-3 flex flex-row justify-between">
+          {
+            goalsAndCompletes && goalsAndCompletes.length > 0 &&
+            <Text className="text-4xl font-bold">{ goalsAndCompletes[0].name }</Text>
+          }
           <Pressable onPress={ back }>
             <FontAwesome5 name="arrow-left" size={24} color="black" />
           </Pressable>
         </View>
         <View className="mt-4">
+          <Text className="ml-4 text-3xl">Activity</Text>
           <Calendar
-            onDayPress={ onDayPress }
-            onMonthChange={ (month: any) => console.log('month changed', month) }
+            onMonthChange={ (date: DateData) => { fetchGoals(user.id, date.dateString); }}
             dayComponent={({ date, index }: { date: string, index:  number }) => renderDay(date, index)}
           />
         </View>
