7ac2e68c2dbf1ff6950d0c1c381abf2e26315259
Richard Chou <konami99@hotmail.com>
2025-02-01 22:35:39 +1100

    delete habits

--- app/(main)/goal/[id].tsx
+++ app/(main)/goal/[id].tsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable } from "react-native"
+import { View, Text, Pressable, ScrollView } from "react-native"
 import BackButton from "@/components/BackButton"
 import { useRouter } from "expo-router";
 import { Calendar, CalendarUtils, DateData } from 'react-native-calendars';
@@ -25,6 +25,7 @@ const GoalEdit = () => {
   const [goalsAndCompletes, setGoalsAndCompletes] = useState([])
   const { id } = useLocalSearchParams<{ id: string }>();
   const [updateKey, setUpdateKey] = useState(0)
+  const [confirmDelete, setConfirmDelete] = useState(false);
 
   const getDate = (count: number) => {
     console.log('getdate')
@@ -93,63 +94,114 @@ const GoalEdit = () => {
     return goalsAndCompletes && goalsAndCompletes.length > 0 ? goalsAndCompletes[0] : undefined;
   }
 
+  const deleteHabit = async () => {
+    console.log('goalsAndCompletes', goalsAndCompletes.length)
+    if (goalsAndCompletes.length > 0) {
+      const goal = goalsAndCompletes[0];
+
+      const { error } = await supabase
+        .from('Goal')
+        .update({ enabled: false })
+        .eq('id', goal.id)
+
+      if (error) console.log(`delete error: `, error)
+      else {
+        console.log('dismiss')
+        router.push(`/(main)/home`);
+      }
+    }
+  }
+
   return (
     <ScreenWrapper bg={"white"}>
-      <View className="m-4 mt-2">
-        <View className="m-3 flex flex-row justify-between">
+      <ScrollView
+        showsHorizontalScrollIndicator={ false }
+        showsVerticalScrollIndicator={ false }
+      >
+        <View className="m-4 mt-2">
+          <View className="m-3 flex flex-row justify-between">
+            {
+              goal() &&
+              <Text className="text-4xl font-bold">{ getHabitFromId(goal()!.name) }</Text>
+            }
+            <Pressable onPress={ back }>
+              <FontAwesome5 name="arrow-left" size={24} color="black" />
+            </Pressable>
+          </View>
+          <View className="mt-4">
+            <Text className="mx-4 text-3xl">Activity</Text>
+            <Calendar
+              onMonthChange={ (date: DateData) => { fetchGoals(user.id, date.dateString); }}
+              dayComponent={({ date, index }: { date: string, index:  number }) => renderDay(date, index)}
+              theme={{
+                arrowColor: 'black',
+                'stylesheet.calendar.header': {
+                  header: {
+                    flexDirection: 'row',
+                    justifyContent: 'start',
+                    paddingLeft: 0,
+                    paddingRight: 10,
+                    marginTop: 6,
+                    alignItems: 'center',
+                  },
+                }
+              }}
+              /*
+              renderHeader={ (date: DateData)  => {
+                console.log(date)
+                return <Text>ere</Text>
+              }}
+              */
+            />
+          </View>
+          <View className="mt-4">
+            <Text className="mx-4 text-3xl">Goal</Text>
+            <Pressable onPress={present} className="mx-4 mt-2 py-1 px-4 bg-green-500 rounded-xl flex flex-row items-center justify-between">
+              <Text className="font-bold text-xl">
+                { goal() &&
+                    `${getHabitFromId(goal()!.name)} ${goal()!.frequency} times in ${goal()!.frequencyrange} days`  } 
+              </Text>
+              <FontAwesome5 name="pencil-alt" size={12} color="black" />
+            </Pressable>
+            <Pressable onPress={present} className="mx-4 mt-2 py-1 px-4 bg-green-500 rounded-xl flex flex-row items-center justify-between">
+              <Text className="font-bold text-xl">
+                { goal() &&
+                    `${goal()!.mincount} ${goal()!.minunit}`  } 
+              </Text>
+              <FontAwesome5 name="pencil-alt" size={12} color="black" />
+            </Pressable>
+            <HabitFormSheet sheet={sheet} dismiss={dismiss} goal={goal()} />
+          </View>
+
+          <View className="bg-[#ccc] h-1 my-10 mx-4" />
+          
           {
-            goal() &&
-            <Text className="text-4xl font-bold">{ getHabitFromId(goal()!.name) }</Text>
+            confirmDelete ?
+            <View>
+              <Text className="mx-4 mb-4 text-3xl">Delete Habit?</Text>
+              <View className="flex flex-row justify-start items-center">
+                <Pressable onPress={ deleteHabit } className="mx-4 py-1 h-16 w-28 px-4 bg-green-500 rounded-full flex flex-row items-center justify-center">
+                  <Text className="font-bold text-xl">
+                    YES
+                  </Text>
+                </Pressable>
+                <Pressable onPress={ () => setConfirmDelete(false) } className="mx-4 py-1 h-16 w-28 px-4 bg-green-500 rounded-full flex flex-row items-center justify-center">
+                  <Text className="font-bold text-xl">
+                    NO
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
+            : <View className="flex flex-row items-center justify-center">
+              <Pressable onPress={ () => setConfirmDelete(true) } className="mx-4 py-1 h-16 w-52 px-4 bg-green-500 rounded-full flex flex-row items-center justify-center">
+                <Text className="font-bold text-xl">
+                  DELETE HABIT
+                </Text>
+              </Pressable>
+            </View>
           }
-          <Pressable onPress={ back }>
-            <FontAwesome5 name="arrow-left" size={24} color="black" />
-          </Pressable>
         </View>
-        <View className="mt-4">
-          <Text className="mx-4 text-3xl">Activity</Text>
-          <Calendar
-            onMonthChange={ (date: DateData) => { fetchGoals(user.id, date.dateString); }}
-            dayComponent={({ date, index }: { date: string, index:  number }) => renderDay(date, index)}
-            theme={{
-              arrowColor: 'black',
-              'stylesheet.calendar.header': {
-                header: {
-                  flexDirection: 'row',
-                  justifyContent: 'start',
-                  paddingLeft: 0,
-                  paddingRight: 10,
-                  marginTop: 6,
-                  alignItems: 'center',
-                },
-              }
-            }}
-            /*
-            renderHeader={ (date: DateData)  => {
-              console.log(date)
-              return <Text>ere</Text>
-            }}
-            */
-          />
-        </View>
-        <View className="mt-4">
-          <Text className="mx-4 text-3xl">Goal</Text>
-          <Pressable onPress={present} className="mx-4 mt-2 py-1 px-4 bg-green-500 rounded-xl flex flex-row items-center justify-between">
-            <Text className="font-bold text-xl">
-              { goal() &&
-                  `${getHabitFromId(goal()!.name)} ${goal()!.frequency} times in ${goal()!.frequencyrange} days`  } 
-            </Text>
-            <FontAwesome5 name="pencil-alt" size={12} color="black" />
-          </Pressable>
-          <Pressable onPress={present} className="mx-4 mt-2 py-1 px-4 bg-green-500 rounded-xl flex flex-row items-center justify-between">
-            <Text className="font-bold text-xl">
-              { goal() &&
-                  `${goal()!.mincount} ${goal()!.minunit}`  } 
-            </Text>
-            <FontAwesome5 name="pencil-alt" size={12} color="black" />
-          </Pressable>
-          <HabitFormSheet sheet={sheet} dismiss={dismiss} goal={goal()} />
-        </View>
-      </View>
+      </ScrollView>
     </ScreenWrapper>
   )
 }
